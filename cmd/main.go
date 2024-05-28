c3de14cfe955ed22051f01076ce0fffdd3b2bc76
jmarren <jmarren99@gmail.com>
2024-05-27 17:02:44 -0700

    loaded alien, old man, and tiles

--- cmd/main.go
+++ cmd/main.go
@@ -16,10 +16,13 @@ type Image struct {
 	ScaleX   float64
 	ScaleY   float64
 	Rotation float64
+	ResX     float64
+	ResY     float64
 }
 
 type Game struct {
-	Images []Image
+	Images     []Image
+	Background Image
 }
 
 func init() {
@@ -36,15 +39,25 @@ func updateCharacterPositions(g *Game) {
 	// Example of moving the first image with 'A' and 'D' keys
 	if len(g.Images) > 0 {
 		if ebiten.IsKeyPressed(ebiten.KeyA) {
-			g.Images[0].X -= 2
+			g.Images[1].X -= 2
 		}
 		if ebiten.IsKeyPressed(ebiten.KeyD) {
-			g.Images[0].X += 2
+			g.Images[1].X += 2
 		}
 	}
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	numberOfTilesX := 1280 / 16
+	numberOfTilesY := 960 / 16
+	for i := 0; i < numberOfTilesX; i++ {
+		for j := 0; j < numberOfTilesY; j++ {
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(float64(i*16), float64(j*16))
+			op.GeoM.Scale(4, 4)
+			screen.DrawImage(g.Background.Img, op)
+		}
+	}
 	for _, img := range g.Images {
 		if img.Img == nil {
 			continue
@@ -66,9 +79,13 @@ func main() {
 
 	// Define the images with their paths and options
 	g.Images = []Image{
-		{Path: "../background.png", X: 0, Y: 0, ScaleX: 6.5, ScaleY: 5, Rotation: 0},
-		{Path: "../New Piskel-1.png.png", X: 20, Y: 20, ScaleX: 4, ScaleY: 4, Rotation: 0},
-		// Add more images as needed
+		{Path: "../assets/OldMan1.png", X: 40, Y: 20, ScaleX: 4, ScaleY: 4, Rotation: 0, ResX: 32, ResY: 32},
+		{Path: "../New Piskel-1.png.png", X: 20, Y: 20, ScaleX: 4, ScaleY: 4, Rotation: 0, ResX: 32, ResY: 32},
+	}
+
+	// Define the background
+	g.Background = Image{
+		Path: "../assets/GrayTile1.png", X: 0, Y: 0, ScaleX: 4, ScaleY: 4, Rotation: 0, ResX: 16, ResY: 16,
 	}
 
 	// Load images
@@ -80,97 +97,15 @@ func main() {
 		g.Images[i].Img = img
 	}
 
+	tile, _, err := ebitenutil.NewImageFromFile(g.Background.Path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	g.Background.Img = tile
+
 	ebiten.SetWindowSize(1280, 960)
 	ebiten.SetWindowTitle("Render multiple images with different options")
 	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
 }
-
-// var (
-// 	background   *ebiten.Image
-// 	img          *ebiten.Image
-// 	charX, charY float64 = 0, 0 // Character's position
-// )
-//
-// type Image struct {
-// 	Img      *ebiten.Image
-// 	X, Y     float64
-// 	ScaleX   float64
-// 	ScaleY   float64
-// 	Rotation float64
-// }
-//
-// func init() {
-// }
-//
-// type Game struct {
-// 	Images []Image
-// }
-//
-// func updateCharacterPosition() {
-// 	if ebiten.IsKeyPressed(ebiten.KeyA) {
-// 		charX -= 2
-// 	}
-// 	if ebiten.IsKeyPressed(ebiten.KeyD) {
-// 		charX += 2
-// 	}
-// 	if ebiten.IsKeyPressed(ebiten.KeyW) {
-// 		charY -= 2
-// 	}
-// }
-//
-// func (g *Game) Draw(screen *ebiten.Image) {
-// for _, img := range g.Images {
-// 		if img.Img == nil {
-// 			continue
-// 		}
-// 		op := &ebiten.DrawImageOptions{}
-// 		op.GeoM.Translate(img.X, img.Y)
-// 		op.GeoM.Scale(img.ScaleX, img.ScaleY)
-// 		op.GeoM.Rotate(img.Rotation)
-// 		screen.DrawImage(img.Img, op)
-// 	}
-// }
-//
-// func (g *Game) Update() error {
-// 	updateCharacterPosition()
-// 	return nil
-// }
-//
-// func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-// 	return 640, 480
-// }
-//
-// func main() {
-// 	ebiten.SetWindowSize(1280, 960)
-// 	ebiten.SetWindowTitle("Render an image")
-// 	g := &Game{}
-// 	g.Images = loadImages()
-// 	if err := ebiten.RunGame(g); err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
-//
-// func loadImages() []Image {
-// 	var images []Image
-//
-// 	// Load first image
-// 	img1, _, err := ebitenutil.NewImageFromFile("../New Piskel-1.png.png")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	images = append(images, Image{Img: img1, X: 0, Y: 0})
-//
-// 	// Load second image
-// 	img2, _, err := ebitenutil.NewImageFromFile("../background.png")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	images = append(images, Image{Img: img2, X: 100, Y: 100})
-//
-// 	// Add more images as needed
-// 	// ...
-//
-// 	return images
-// }
